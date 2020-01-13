d698f0d57bb65e4f654dda6cc85d6cc75aac1c85
Igor Savin <kibertoad@gmail.com>
2020-01-14 00:24:34 +0100

    Support multiple assertions

--- lib/execution-test-helper.ts
+++ lib/execution-test-helper.ts
@@ -14,6 +14,61 @@ declare interface ExecResult {
   stderr: string
 }
 
+function toStringArray(value?: string | string[]): string[] | undefined {
+  if (typeof value === 'string') {
+    return [value]
+  }
+  return value
+}
+
+function assertErrorMessages(msg: string, expectedErrorMessage: string[] | string | undefined, rejectFn: Function) {
+  const expectedErrorMessages = toStringArray(expectedErrorMessage)
+
+  const errors: string[] = []
+  if (expectedErrorMessages && expectedErrorMessages.length > 0) {
+    expectedErrorMessages.forEach((expectedEntry: string) => {
+      if (!msg.includes(expectedEntry)) {
+        errors.push(`Expected error to include "${expectedEntry}", but it was actually "${msg}"`)
+      }
+    })
+  }
+  if (errors.length > 0) {
+    rejectFn(new AssertionError(errors.join('\n')))
+  }
+}
+
+function assertOutput(stdout: string, expectedOutput: string[] | string | undefined, rejectFn: Function) {
+  const expectedOutputs = toStringArray(expectedOutput)
+
+  const errors: string[] = []
+  if (expectedOutputs && expectedOutputs.length > 0) {
+    expectedOutputs.forEach((expectedEntry: string) => {
+      if (!stdout.includes(expectedEntry)) {
+        errors.push(`Expected output to include "${expectedEntry}", but it was actually "${stdout}"`)
+      }
+    })
+  }
+  if (errors.length > 0) {
+    rejectFn(new AssertionError(errors.join('\n')))
+  }
+}
+
+function assertNotOutput(stdout: string, notExpectedOutput: string[] | string | undefined, rejectFn: Function) {
+  const notExpectedOutputs = toStringArray(notExpectedOutput)
+
+  const errors: string[] = []
+  if (notExpectedOutputs && notExpectedOutputs.length > 0) {
+    notExpectedOutputs.forEach((notExpectedEntry: string) => {
+      if (stdout.includes(notExpectedEntry)) {
+        errors.push(`Expected output not to include "${notExpectedEntry}", but it was actually "${stdout}"`)
+      }
+    })
+  }
+  if (errors.length > 0) {
+    rejectFn(new AssertionError(errors.join('\n')))
+  }
+}
+
 /**
  * Execute given commandline command
  */
@@ -26,9 +81,9 @@ export function execCommand(
     notExpectedOutput
   }: {
     description?: string
-    expectedErrorMessage?: string
-    expectedOutput?: string
-    notExpectedOutput?: string
+    expectedErrorMessage?: string | string[]
+    expectedOutput?: string | string[]
+    notExpectedOutput?: string | string[]
   } = {}
 ): Promise<ExecResult> {
   description = description || cliCommand
@@ -39,9 +94,7 @@ export function execCommand(
     bin.addListener('error', (msg: string, _code: any) => {
       // Error is expected
       if (expectedErrorMessage) {
-        if (!msg.includes(expectedErrorMessage)) {
-          throw new AssertionError(`Expect error to include "${expectedErrorMessage}", but it was actually "${msg}"`)
-        }
+        assertErrorMessages(msg, expectedErrorMessage, reject)
         return resolve({ stdout, stderr })
       }
 
@@ -54,18 +107,12 @@ export function execCommand(
       }
 
       if (expectedOutput) {
-        if (!stdout.includes(expectedOutput)) {
-          throw new AssertionError(`Expected output to include "${expectedOutput}", but it was actually "${stdout}"`)
-        }
+        assertOutput(stdout, expectedOutput, reject)
         return resolve({ stdout, stderr })
       }
 
       if (notExpectedOutput) {
-        if (stdout.includes(notExpectedOutput)) {
-          throw new AssertionError(
-            `Expected output not to include "${notExpectedOutput}", but it was actually "${stdout}"`
-          )
-        }
+        assertNotOutput(stdout, notExpectedOutput, reject)
         return resolve({ stdout, stderr })
       }
 
--- test/integration/suite/execution-test-helper.integration.spec.ts
+++ test/integration/suite/execution-test-helper.integration.spec.ts
@@ -5,20 +5,50 @@ const pathToApp = path.normalize(__dirname + '/../../util/apps/basic.cli.app.js'
 
 describe('execution-test-helper', () => {
   it('executes with an error', async () => {
-    return execCommand(`node ${pathToApp} error Kaboom`, {
+    await execCommand(`node ${pathToApp} error Kaboom`, {
       expectedErrorMessage: 'Kaboom'
     })
   })
 
+  it('supports multiple error assertions', async () => {
+    await execCommand(`node ${pathToApp} error Crash-and-burn`, {
+      expectedErrorMessage: ['Crash', 'burn']
+    })
+  })
+
   it('executes without an error', async () => {
-    return execCommand(`node ${pathToApp} message OK`, {
+    await execCommand(`node ${pathToApp} message OK`, {
       expectedOutput: 'OK'
     })
   })
 
+  it('supports multiple assertions', async () => {
+    await execCommand(`node ${pathToApp} message OK-and-fine`, {
+      expectedOutput: ['OK', 'fine']
+    })
+  })
+
+  it('throws an error if not expected text exists', async () => {
+    expect.assertions(1)
+
+    try {
+      await execCommand(`node ${pathToApp} message OK`, {
+        notExpectedOutput: 'OK'
+      })
+    } catch (err) {
+      expect(err.message.startsWith('Expected output not to include "OK", but it was actually "OK')).toBeTruthy()
+    }
+  })
+
   it('does not include unexpected text', async () => {
-    return execCommand(`node ${pathToApp} message OK`, {
+    await execCommand(`node ${pathToApp} message OK`, {
       notExpectedOutput: 'error'
+    })
+  })
+
+  it('supports multiple negative assertions', async () => {
+    await execCommand(`node ${pathToApp} message OK-and-fine`, {
+      notExpectedOutput: ['error', 'warning']
     })
   })
 })
